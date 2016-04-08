4131b397de885f5c2ca96d077f6e4063dcf213be
Dan van Kley <dvankley@salesforce.com>
2016-04-08 11:01:41 -0400

    Pass in team and player number

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -22,8 +22,13 @@ class PlayersController < ApplicationController
       ::WebsocketRails[:"room#{@id}"].trigger 'user_scan_new', player.id
     else
       # Otherwise this player exists, so add them to this room's current players
-      add_new_player(@id, player)
-      ::WebsocketRails[:"room#{@id}"].trigger 'user_scan_existing', { :player_id => player.id, :image_url => player.image_url}
+      room_player = add_new_player(@id, player)
+      ::WebsocketRails[:"room#{@id}"].trigger 'user_scan_existing', {
+          :player_id => player.id,
+          :image_url => player.image_url,
+          :team => room_player.team,
+          :player_number => room_player.player_number
+      }
     end
 
     render nothing: true
@@ -105,6 +110,7 @@ class PlayersController < ApplicationController
     end
 
     room_player.save
+    room_player
   end
 
   def set_player
