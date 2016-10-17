8217dfeb5a2bab4923e3e1c040705207cf79f7bf
Dan van Kley <dvankley@salesforce.com>
2016-10-17 15:39:31 -0400

    Twiddle stats parameters a little

--- lib/rating_manager.rb
+++ lib/rating_manager.rb
@@ -8,16 +8,21 @@ class RatingManager
   TRUESKILL_MU = 25.0
   # Default initial standard deviation of ratings.
   TRUESKILL_SIGMA = 8.333333333333334
-  # Default distance that guarantees about 76% chance of winning.
-  TRUESKILL_BETA = 4.166666666666667
+  # Default distance 4.166666 guarantees about 76% chance of winning.
+  # Small element of chance in ping pong, so go lower
+  TRUESKILL_BETA = 2.0
   # Default dynamic factor.
   TRUESKILL_TAU = 0.08333333333333334
+  # Variance of score distribution. should be extracted from user data. For games with few scoring events (Soccer, etc)
+  #   gamma is small, for games with many scoring events (Shooter, etc), gamma is large
+  #   Defaults to 0.1
+  TRUESKILL_GAMMA = 4.0
   # Default draw probability of the game.
-  TRUESKILL_DRAW_PROBABILITY = 0.1
+  TRUESKILL_DRAW_PROBABILITY = 0.0
 
   def initialize
     @beta = TRUESKILL_BETA
-    @gamma = 0.5
+    @gamma = TRUESKILL_GAMMA
   end
 
   # @param player [Player]
@@ -48,7 +53,7 @@ class RatingManager
     # team 1 wins by 10 points against team 2
     graph = ScoreBasedBayesianRating.new(
         {team_1_ratings => team_1_score_differential, team_2_ratings => team_2_score_differential},
-        {:beta => @beta, :gamma => @gamma}
+        {:beta => @beta, :gamma => @gamma, :skills_additive => false}
     )
 
     # update the Ratings
