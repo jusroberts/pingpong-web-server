cb245665f9d7974cbb3a59d2e14e58bdda54163f
Justin <justin.roberts@salesforce.com>
2017-01-23 13:01:48 -0500

    exception to request forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, unless: -> { request.format.json? }
   force_ssl if: :ssl_configured?
   before_action :hide_topbar
 
