d10c495060c40ab3b41f2ded3681af1da3a42d67
Justin <justin.roberts@salesforce.com>
2017-01-23 13:10:16 -0500

    not forced SSL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
-  force_ssl if: :ssl_configured?
+  # force_ssl if: :ssl_configured?
   before_action :hide_topbar
 
   def ssl_configured?
